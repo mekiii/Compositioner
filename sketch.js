da65fa42c3fd68ec259b2214eae4a43703afd6b4
Mekong Lam <mekong.lam@gmail.com>
2019-12-03 17:17:12 +0100

    Adapt canvas size

--- sketch.js
+++ sketch.js
@@ -22,41 +22,49 @@ let goldenX1;
 let goldenX2;
 let goldenY;
 let randomPoints = [];
+let wWidth, wHeight;
 
 function setup() {
-  createCanvas(windowWidth, windowHeight);
+  wWidth = Math.round(random(400,windowWidth - 10));
+  wHeight = Math.round(random(400, windowHeight - 40));
+  createCanvas(wWidth, wHeight);
   gMean = (sqrt(5) + 1 )/2
-  goldenX1 = windowWidth/(gMean+1);
-  goldenX2 = windowWidth*gMean/(gMean+1);
-  goldenY = windowHeight*gMean/(gMean+1);
+  goldenX1 = wWidth/(gMean+1);
+  goldenX2 = wWidth*gMean/(gMean+1);
+  goldenY = wHeight*gMean/(gMean+1);
   let NumOfPoints = Math.round(random(2,7));
   for (let i = 0; i < NumOfPoints; i++){
-    let randomX = Math.round(random(120,windowWidth - 120));
-    let randomY = Math.round(random(120,windowHeight - 120));
-    randomPoints.push([randomX,randomY]);
+    if(i==0){
+      let randomX = randomGaussian(goldenX1, 100);
+      let randomY = randomGaussian(goldenY, 100);
+      randomPoints.push([randomX,randomY]);
+    }
+    else{
+      let randomX = Math.round(random(120,wWidth - 120));
+      let randomY = Math.round(random(120,wHeight - 120));
+      randomPoints.push([randomX,randomY]);
+    }
   }
-  randomPoints
 }
 
 function draw() {
   background(255);
   stroke(0,10);
   strokeWeight(2);
-  line(goldenX1,0, goldenX1, windowHeight);
-  line(0, goldenY, windowWidth, goldenY);
+  line(goldenX1,0, goldenX1, wHeight);
+  line(0, goldenY, wWidth, goldenY);
   stroke(0,200);
   noFill();
   randomPoints.forEach(function(value, index, arrayObj) {
     point(value[0],value[1]);
     if(index == 0){
-      //stroke(125,149,164, 150);
       noStroke();
-      fill(252,202,104, 30);
+      fill(252,202,104, 50);
       ellipse(value[0], value[1], 40, 40);
       ellipse(value[0], value[1], 120, 120);
       stroke(125,149,164, 150);
-      line(value[0], 0, value[0], windowHeight);
-      line(0, value[1],  windowWidth,value[1]);
+      line(value[0], 0, value[0], wHeight);
+      line(0, value[1], wWidth,value[1]);
       textSize(14);
       noStroke();
       fill(125,149,164, 150)
